a59f3e6972313a984ccbad609e419acd0fa07858
Liam Ozog <liam@ada.support>
2021-12-01 09:45:13 -0500

    reorganize folder structure

---
+++ 01/star 2/main.py
@@ -0,0 +1,25 @@
+input_file = open('input.txt', 'r')
+
+increase_count = 0
+prev_line = None
+ 
+while True:
+    # Get next line from file
+    line = input_file.readline()
+
+    # if line is empty
+    # end of file is reached
+    if not line:
+        break
+
+    line_num = int(line)
+
+    if prev_line is not None:
+        if line_num > prev_line:
+            increase_count += 1
+            
+    prev_line = line_num
+ 
+input_file.close()
+
+print(increase_count)
