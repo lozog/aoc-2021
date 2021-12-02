8dbdd14f88aa3a1a2f082483b621001cde5f2f25
Liam Ozog <liam@ada.support>
2021-12-02 11:09:05 -0500

    day 02 star 2

--- day02.py
+++ day02.py
@@ -1,4 +1,4 @@
-DEBUG = True
+DEBUG = False
 
 def log(*args):
     if DEBUG:
@@ -33,4 +33,36 @@ def p1():
     res = h_pos * depth
     print(f"p1: {res}")
     
+def p2():
+    input_file = open('day02_input_full.txt', 'r')
+
+    h_pos = 0
+    depth = 0
+    aim = 0
+
+    while True:
+        line = input_file.readline()
+        if not line:
+            break
+
+        command = line.split()
+        direction = command[0]
+        distance = int(command[1])
+        log(direction, distance)
+
+        if direction == "forward":
+            h_pos += distance
+            depth += distance * aim
+        elif direction == "down":
+            aim += distance
+        elif direction == "up":
+            aim -= distance
+
+    input_file.close()
+
+    log(h_pos, depth)
+    res = h_pos * depth
+    print(f"p2: {res}")
+
 p1()
+p2()
