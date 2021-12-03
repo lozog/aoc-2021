2f233ed2f767996a3a78f503200e59ff62ca1e99
Liam Ozog <liam@ada.support>
2021-12-03 02:12:15 -0500

    cleaned up

--- day03.py
+++ day03.py
@@ -20,7 +20,7 @@ def p1():
     # print(gamma_rate, epsilon_rate)
     print(int(gamma_rate, 2) * int(epsilon_rate, 2))
 
-# p1()
+p1()
 
 def p2():
     input_file = open('input/day03_full', 'r')
@@ -28,36 +28,24 @@ def p2():
     report = [list(line) for line in input_file.read().splitlines()] # input as 2D array of chars
     # pprint(report)
 
-    def most_common_digit(data, num_diagnostics):
-        num_ones = sum([int(i) for i in data[0]])
-        return int(num_ones >= (num_diagnostics / 2)) # shoutout to the C programming language
-
-    def least_common_digit(data, num_diagnostics):
-        num_ones = sum([int(i) for i in data[0]])
-        return int(not num_ones >= (num_diagnostics / 2)) # shoutout to the C programming language
-
-    oxygen_rating = ""
-    co2_rating = ""
-
-    for col in range(0, len(report[0])):
-        filtered_report_oxygen = [row[col:] for row in report if col == 0 or ''.join(row[0:col]) == oxygen_rating]
-        if (len(filtered_report_oxygen) == 1):
-            oxygen_rating += filtered_report_oxygen[0][0]
-        else:
-            num_diagnostics = len(filtered_report_oxygen)
-            transposed_report = list(zip(*filtered_report_oxygen))
-            most_common_digit_in_col = most_common_digit(transposed_report, num_diagnostics)
-            oxygen_rating += str(most_common_digit_in_col)
-
-        filtered_report_co2 = [row[col:] for row in report if col == 0 or ''.join(row[0:col]) == co2_rating]
-        if (len(filtered_report_co2) == 1):
-            co2_rating += filtered_report_co2[0][0]
-        else:
-            num_diagnostics = len(filtered_report_co2)
-            transposed_report = list(zip(*filtered_report_co2))
-            least_common_digit_in_col = least_common_digit(transposed_report, num_diagnostics)
-            co2_rating += str(least_common_digit_in_col)
+    def find_res(bit_criteria):
+        res = ""
+        for col in range(0, len(report[0])):
+            filtered_report = [row[col:] for row in report if col == 0 or ''.join(row[0:col]) == res] # only look at rows whose first $col digits match res
+            num_diagnostics = len(filtered_report)
+            if (num_diagnostics == 1):
+                # base case: if we only have one row left, that's the rest of the result
+                return res + ''.join(filtered_report[0])
+            else:
+                transposed_report = list(zip(*filtered_report))
+                num_ones = sum([int(i) for i in transposed_report[0]]) # count ones in first column
+                res += str(int(bool(bit_criteria) == (num_ones >= (num_diagnostics / 2)))) # more black magic
         
-    print(oxygen_rating, co2_rating)
+        return res
+
+    oxygen_rating = find_res(0)
+    co2_rating = find_res(1)
+        
+    # print(oxygen_rating, co2_rating)
     print(int(oxygen_rating, 2) * int(co2_rating, 2))
 p2()
