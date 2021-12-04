d2aff49bdd3b736d477d871b829d0427b8320a43
Liam Ozog <liam@ada.support>
2021-12-04 14:02:08 -0500

    wip day 4: create copy of boards, mark spaces where input matches

--- day04.py
+++ day04.py
@@ -1,4 +1,7 @@
+from copy import deepcopy
 from pprint import pprint
+
+BINGO_DIM = 5
 
 def process_bingo_input(file):
     """
@@ -6,7 +9,7 @@ def process_bingo_input(file):
     and a list of boards, represented as 2D arrays
     """
     input_file = open(file, 'r')
-    inputs = input_file.readline()
+    inputs = [int(i) for i in input_file.readline().split(",")]
     boards = []
     
     while True:
@@ -16,8 +19,8 @@ def process_bingo_input(file):
             break
 
         board = []
-        for i in range(5):
-            line = [char for char in input_file.readline().split()]
+        for i in range(BINGO_DIM):
+            line = [int(char) for char in input_file.readline().split()]
             board.append(line)
         boards.append(board)
     input_file.close()
@@ -26,7 +29,29 @@ def process_bingo_input(file):
 def p1():
     inputs, boards = process_bingo_input('input/day04_test')
     print(inputs)
-    for board in boards:
-        pprint(board)
+
+    boards_marked = []
+    for i, board in enumerate(boards):
+        boards_marked.append(
+            [
+                ['' for x in range(BINGO_DIM)]
+                for y in range(BINGO_DIM)
+            ]
+        )
+
+    for num in inputs:
+        # print(num)
+        # go through each board, mark boards_marked if it's a match
+        for board_idx, board in enumerate(boards):
+            for x, row in enumerate(board):
+                for y, cell in enumerate(row):
+                    if cell == num:
+                        boards_marked[board_idx][x][y] = 'X'
+
+        # TODO: check for winner
+
+    # for board in boards_marked:
+    #     pprint(board)
+
 
 p1()
