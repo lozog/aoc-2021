963d0f0439f49ab43abd2c53e13348d0fcc96872
Liam Ozog <liam@ada.support>
2021-12-06 18:09:05 -0500

    day 06 star 2

--- day06.py
+++ day06.py
@@ -1,17 +1,28 @@
+from pprint import pprint
+
 input_file = open('input/day06_full', 'r')
 fish = [int(i) for i in input_file.readline().split(",")]
-
 input_file.close()
 
-NUM_DAYS = 80
-# print(f"initial: {fish}")
+NUM_DAYS = 256
 
-for day in range(NUM_DAYS + 1):
-    new_fish = [8 for fish_timer in fish if fish_timer < 0]
-    fish = [6 if fish_timer < 0 else fish_timer for fish_timer in fish ]
-    fish = [*fish, *new_fish]
-    # print(f"after {day} day(s): {fish}")
-    fish = [fish_timer - 1 for fish_timer in fish]
+# keys are days left until next spawn (aka timer), values are # of fish at that timer value
+fish_map = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
 
-num_fish = len(fish)
-print(f"p1: {num_fish}")
+for fish_timer in fish:
+    fish_map[fish_timer] += 1
+# pprint(fish_map)
+
+for day in range(NUM_DAYS):
+    num_fish_at_0 = 0
+    for timer, fish_count in fish_map.items(): # shift all fish down one spot (i.e. decrement timer for each)
+        if timer == 0:
+            num_fish_at_0 = fish_count
+        else:
+            fish_map[timer - 1] = fish_count
+
+    fish_map[6] += num_fish_at_0
+    fish_map[8] = num_fish_at_0
+# print(fish_map)
+
+print(sum(fish_map.values()))
