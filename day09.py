e8579750c3e6bc643aa62de2f77c20e1cad7f2bc
Liam Ozog <liam@ada.support>
2021-12-09 15:23:09 -0500

    day 09 star 2

--- day09.py
+++ day09.py
@@ -1,5 +1,13 @@
+from collections import defaultdict
+from copy import deepcopy
+from dataclasses import dataclass
 from functools import reduce
 from pprint import pprint
+
+@dataclass
+class Point:
+    x: int
+    y: int
 
 input_file = open('input/day09_full', 'r')
 height_map = [[None, *[int(i) for i in line], None] for line in input_file.read().splitlines()]
@@ -23,18 +31,59 @@ def is_cell_local_min(i, j, cell, height_map):
 
     return cell < min(neighbours)
     
-
-local_mins = []
-for i, row in enumerate(height_map):
-    if i == 0 or i == len(height_map) - 1:
-        # skip first and last row, that's just the None border
-        continue
-    for j, cell in enumerate(row):
-        if j == 0 or j == len(row) - 1:
-            # skip first and last column
+def p1():
+    local_mins = []
+    local_mins_coords = []
+    for i, row in enumerate(height_map):
+        if i == 0 or i == len(height_map) - 1:
+            # skip first and last row, that's just the None border
             continue
-        if is_cell_local_min(i, j, cell, height_map):
-            local_mins.append(cell)
+        for j, cell in enumerate(row):
+            if j == 0 or j == len(row) - 1:
+                # skip first and last column
+                continue
+            if is_cell_local_min(i, j, cell, height_map):
+                local_mins.append(cell)
+                local_mins_coords.append(Point(x=i, y=j))
 
-res = sum(local_mins) + len(local_mins)
-print(res)
+    res = sum(local_mins) + len(local_mins)
+    print(f"p1: {res}")
+    return local_mins_coords
+
+
+def flood_fill(i, j, basin_num, height_map, basins):
+    if height_map[i][j] is None:
+        return
+    if basins[i][j] != '':
+        return
+    if height_map[i][j] < 9:
+        basins[i][j] = basin_num
+        flood_fill(i+1, j, basin_num, height_map, basins )
+        flood_fill(i-1, j, basin_num, height_map, basins )
+        flood_fill(i, j-1, basin_num, height_map, basins )
+        flood_fill(i, j+1, basin_num, height_map, basins )
+
+def p2():
+    local_mins = p1()
+    # print(local_mins)
+    basins = [["" if cell is not None else None for cell in row] for row in height_map]
+    for basin_num, local_min in enumerate(local_mins):
+        flood_fill(local_min.x, local_min.y, basin_num, height_map, basins)
+    # pprint(basins)
+
+    # flatten, remove all Nones, count each unique 
+    basins = [val for row in basins for val in row if val is not None and val != '']
+    # print(basins)
+    basin_sizes = defaultdict(lambda: 0)
+    for point in basins:
+        basin_sizes[point] += 1
+    basin_sizes = list(basin_sizes.values())
+    # print(basin_sizes)
+
+    top_3_basin_sizes = sorted(basin_sizes, reverse=True)[:3]
+    # print(top_3_basin_sizes)
+    res = reduce(lambda a, b: a*b, top_3_basin_sizes)
+    print(f"p2: {res}")
+
+
+p2()
