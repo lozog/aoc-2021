a080142afcb8e8db1336cb60322dd123ad139bdf
Liam Ozog <liam@ada.support>
2021-12-13 23:45:50 -0500

    day 13

--- day13.py
+++ day13.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from copy import deepcopy
 from pprint import pprint
 
 @dataclass
@@ -10,7 +11,7 @@ class Point:
 
 @dataclass
 class Fold:
-    dim: int
+    dim: str
     value: int
     def __repr__(self):
         return f"{self.dim}={self.value}"
@@ -45,28 +46,12 @@ def parse_file(file: str):
             dots.append(dot)
         else:
             fold_input = line.split()[2].split("=")
-            fold = Fold(dim = fold_input[0], value = fold_input[1])
+            fold = Fold(dim = fold_input[0], value = int(fold_input[1]))
             folds.append(fold)
             
 
     input_file.close()
     return dots, folds, max_x, max_y
-
-dots, folds, max_x, max_y = parse_file("input/day13_test")
-
-pprint(dots)
-pprint(folds)
-print(max_x)
-print(max_y)
-
-dot_map = [
-    [None for y in range(max_y+1)]
-    for x in range(max_x+1)
-]
-
-
-for dot in dots:
-    dot_map[dot.x][dot.y] = dot
 
 def print_matrix(matrix):
     for row in transpose(matrix):
@@ -77,4 +62,61 @@ def print_matrix(matrix):
             print(char, end="")
         print()
 
+# print_matrix(dot_map)
+# print()
+
+def merge_matrices(matrix1, matrix2):
+    # assumes matrices are same size
+    for x, row in enumerate(matrix1):
+        for y, dot in enumerate(row):
+            if matrix2[x][y] is None:
+                matrix2[x][y] = dot
+    return matrix2
+
+
+dots, folds, max_x, max_y = parse_file("input/day13_full")
+
+# pprint(dots)
+# pprint(folds)
+# print(max_x)
+# print(max_y)
+
+dot_map = [
+    [None for y in range(max_y+1)]
+    for x in range(max_x+1)
+]
+
+for dot in dots:
+    dot_map[dot.x][dot.y] = dot
+
+for fold in folds:
+    value = fold.value
+    # print(fold)
+    if fold.dim == "x":
+        left = dot_map[:value]
+        right = dot_map[value+1:]
+        right = right[::-1]
+        # right=dot_map[value+1:]
+        # print_matrix(left)
+        # print()
+        # print_matrix(right)
+        dot_map = merge_matrices(left, deepcopy(right))
+    else:
+        transposed_map = transpose(dot_map)
+        top = transposed_map[:value]
+        bottom = reversed(transposed_map[value+1:])
+        top = transpose(top)
+        bottom = transpose(bottom)
+        
+        dot_map = merge_matrices(top, deepcopy(bottom))
+
+        # print_matrix(top)
+        # print()
+        # print_matrix(bottom)
+        # print()
+        # print_matrix(dot_map)
+    # break # uncomment for p1
 print_matrix(dot_map)
+
+res = sum([sum([1 for dot in row if dot is not None]) for row in dot_map])
+print(res)
