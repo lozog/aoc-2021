189e653f3f4139b38c0217cccc4d89b8ca94fa95
Liam Ozog <liam@ada.support>
2021-12-18 20:00:33 -0500

    add list of snail numbers

--- day18.py
+++ day18.py
@@ -52,6 +52,7 @@ def add_snails(s1, s2):
     new_snail.r.parent = new_snail
     return new_snail
 
+
 def find_leftmost_at_level_4(snail, level=0):
     if snail is None or snail.val is not None:
         return None
@@ -66,17 +67,29 @@ def find_leftmost_at_level_4(snail, level=0):
             return res
     return None
 
+def find_leftmost_gte_10(snail):
+    if snail.val is not None:
+        return snail._id if snail.val >= 10 else None
+    else:
+        res = find_leftmost_gte_10(snail.l)
+        if res is None:
+            return find_leftmost_gte_10(snail.r)
+        return res
+
+
 def leftmost_leaf(snail):
     if snail.val is not None:
         return snail._id
     else:
         return leftmost_leaf(snail.l)
 
+
 def rightmost_leaf(snail):
     if snail.val is not None:
         return snail._id
     else:
         return rightmost_leaf(snail.r)
+
 
 # climb tree until you don't come from the parent's R, then go down there and find the leftmost left
 def right_neighbour(snail):
@@ -88,6 +101,7 @@ def right_neighbour(snail):
     else:
         return leftmost_leaf(snail.parent.r)
 
+
 def left_neighbour(snail):
     if snail.parent.l._id == snail._id:
         if snail.parent.parent is None:
@@ -97,38 +111,31 @@ def left_neighbour(snail):
         return rightmost_leaf(snail.parent.l)
 
 
-# test1 = snail_from_list([[[[4,3],4],4],[7,[[8,4],9]]])
-# test2 = snail_from_list([1,1])
-# test3 = add_snails(test1, test2)
-# test, snails = snail_from_list([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])
-# test, snails = snail_from_list([7,[6,[5,[4,[3,2]]]]])
-# test, snails = snail_from_list([[6,[5,[4,[3,2]]]],1])
-# test, snails = snail_from_list([[[[[9,8],1],2],3],4])
-test, snails = snail_from_list([[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]])
-print(test)
-
-def explode(snail_id, snails):
-    snail = snails[snail_id]
-    leftmost_id = find_leftmost_at_level_4(snail)
+def explode(leftmost_id, snails):
     leftmost_snail = snails[leftmost_id]
     parent = leftmost_snail.parent
-
-    # remove leftmost
+    # delete leftmost
     snails.pop(leftmost_id)
     snails.pop(leftmost_snail.r._id)
     snails.pop(leftmost_snail.l._id)
+
+    # create new snail with val=0
     new_child = Snail(uuid4(), val=0, parent=parent)
     snails[new_child._id] = new_child
+
+    # set it as the correct child of the parent
     if parent.l._id == leftmost_id:
         parent.l = new_child
     else:
         parent.r = new_child
 
+    # take the left value and apply it to the left neighbour, if there is one
     left_neighbour_id = left_neighbour(new_child)
     if left_neighbour_id is not None:
         left_neighbour_snail = snails[left_neighbour_id]
         left_neighbour_snail.val += leftmost_snail.l.val
 
+    # take the right value and apply it to the right neighbour, if there is one
     right_neighbour_id = right_neighbour(new_child)
     if right_neighbour_id is not None:
         right_neighbour_snail = snails[right_neighbour_id]
@@ -136,13 +143,59 @@ def explode(snail_id, snails):
 
 # explode(test._id, snails)
 
-def split(snail_id, snails):
-    snail = snails[snail_id]
+def split(root_id, snails):
+    snail = snails[root_id]
     snail.l = Snail(uuid4(), val=floor(snail.val/2), parent=snail)
     snail.r = Snail(uuid4(), val=ceil(snail.val/2), parent=snail)
     snail.val = None
     snails[snail.l._id] = snail.l
     snails[snail.r._id] = snail.r
 
-print(test)
-pprint(snails)
+# returns True if an action was performed
+def reduce_once(root_id, snails):
+    snail = snails[root_id]
+    leftmost_at_level_4 = find_leftmost_at_level_4(snail)
+    if leftmost_at_level_4 is not None:
+        explode(leftmost_at_level_4, snails)
+        return True
+    
+    leftmost_gte_10 = find_leftmost_gte_10(snail)
+    if leftmost_gte_10 is not None:
+        split(leftmost_gte_10, snails)
+        return True
+    return False
+
+test_input = [
+    [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]],
+    [7,[[[3,7],[4,3]],[[6,3],[8,8]]]],
+    [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]],
+    [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]],
+    [7,[5,[[3,8],[1,4]]]],
+    [[2,[2,2]],[8,[8,1]]],
+    [2,9],
+    [1,[[[9,3],9],[[9,0],[0,7]]]],
+    [[[5,[7,4]],7],1],
+    [[[[4,2],2],6],[8,7]],
+]
+
+test_snails = []
+snails = dict()
+for snail_input in test_input:
+    new_snail, new_snails = snail_from_list(snail_input)
+    test_snails.append(new_snail)
+    snails = {**snails, **new_snails}
+
+root = test_snails[0]
+for i, next_snail in enumerate(test_snails):
+    if i == 0:
+        continue
+    root = add_snails(root, next_snail)
+    snails[root._id] = root
+    while True:
+        # print(root)
+        res = reduce_once(root._id, snails)
+        if not res:
+            break
+
+print(root)
+# pprint(snails)
