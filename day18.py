386ceda92b630cca5a45c54c3afc8e7c1f622a4a
Liam Ozog <liam@ada.support>
2021-12-18 19:00:37 -0500

    snail number explode

--- day18.py
+++ day18.py
@@ -10,6 +10,7 @@ class Snail:
     l: Type["Snail"] = None
     r: Type["Snail"] = None
     parent: Type["Snail"] = None
+
     def __repr__(self):
         if self.val is not None:
             return f"{self.val}"
@@ -64,13 +65,75 @@ def find_leftmost_at_level_4(snail, level=0):
             return res
     return None
 
+def leftmost_leaf(snail):
+    if snail.val is not None:
+        return snail._id
+    else:
+        return leftmost_leaf(snail.l)
 
-# test1 = snail_from_list([[[[[9,8],1],2],3],4])
+def rightmost_leaf(snail):
+    if snail.val is not None:
+        return snail._id
+    else:
+        return rightmost_leaf(snail.r)
+
+# climb tree until you don't come from the parent's R, then go down there and find the leftmost left
+def right_neighbour(snail):
+    # parent = snail.parent
+    if snail.parent.r._id == snail._id:
+        if snail.parent.parent is None:
+            return None
+        return right_neighbour(snail.parent)
+    else:
+        return leftmost_leaf(snail.parent.r)
+
+def left_neighbour(snail):
+    if snail.parent.l._id == snail._id:
+        if snail.parent.parent is None:
+            return None
+        return left_neighbour(snail.parent)
+    else:
+        return rightmost_leaf(snail.parent.l)
+
+
 # test1 = snail_from_list([[[[4,3],4],4],[7,[[8,4],9]]])
 # test2 = snail_from_list([1,1])
 # test3 = add_snails(test1, test2)
-test3, snails = snail_from_list([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])
-test3, snails = snail_from_list([7,[6,[5,[4,[3,2]]]]])
-print(test3)
-leftmost_id = find_leftmost_at_level_4(test3)
-leftmost_snail = snails[leftmost_id]
+# test, snails = snail_from_list([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])
+# test, snails = snail_from_list([7,[6,[5,[4,[3,2]]]]])
+# test, snails = snail_from_list([[6,[5,[4,[3,2]]]],1])
+# test, snails = snail_from_list([[[[[9,8],1],2],3],4])
+test, snails = snail_from_list([[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]])
+print(test)
+
+# explode
+def explode(snail_id, snails):
+    snail = snails[snail_id]
+    leftmost_id = find_leftmost_at_level_4(snail)
+    leftmost_snail = snails[leftmost_id]
+    parent = leftmost_snail.parent
+
+    # remove leftmost
+    snails.pop(leftmost_id)
+    snails.pop(leftmost_snail.r._id)
+    snails.pop(leftmost_snail.l._id)
+    new_child = Snail(uuid4(), val=0, parent=parent)
+    snails[new_child._id] = new_child
+    if parent.l._id == leftmost_id:
+        parent.l = new_child
+    else:
+        parent.r = new_child
+
+    left_neighbour_id = left_neighbour(new_child)
+    if left_neighbour_id is not None:
+        left_neighbour_snail = snails[left_neighbour_id]
+        left_neighbour_snail.val += leftmost_snail.l.val
+
+    right_neighbour_id = right_neighbour(new_child)
+    if right_neighbour_id is not None:
+        right_neighbour_snail = snails[right_neighbour_id]
+        right_neighbour_snail.val += leftmost_snail.r.val
+
+explode(test._id, snails)
+print(test)
+# pprint(snails)
