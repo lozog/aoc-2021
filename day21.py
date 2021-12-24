17986492db39c07e7b48e5a08671177f2afb49e9
Liam Ozog <liam@ada.support>
2021-12-24 01:03:36 -0500

    day 21 star 2 - works up to 6

--- day21.py
+++ day21.py
@@ -44,7 +44,6 @@ for line in lines:
 
 
 # p2
-END_SCORE = 3
 rolls = defaultdict(int)
 for i in range(3):
     for j in range(3):
@@ -54,6 +53,8 @@ rolls = dict(rolls)
 
 print(players)
 
+END_SCORE = 7
+
 # 2d array of all possible positions and all possbile scores
 # e.g. the value at all_p1_scores[x][y] is the # of universes where p1 has score y at position x after a certain number of turns
 all_p0_scores = [[0 for i in range(END_SCORE)] for position in range(10+1)]
@@ -61,14 +62,18 @@ all_p1_scores = [[0 for i in range(END_SCORE)] for position in range(10+1)]
 
 all_p0_scores[players[0]][0] = 1
 all_p1_scores[players[1]][0] = 1
-all_player_scores = all_p0_scores
 
 num_winning_universes = [0 for player in players]
 player = 0
-while True :
-    print(player)
+all_player_scores = all_p0_scores
+while True:
+    # print(player)
     new_scores = [[0 for i in range(END_SCORE)] for position in range(10+1)]
-
+    if player == 0:
+        num_remaining_opponents = sum([num_at_score for position in all_p1_scores for num_at_score in position])
+    else:
+        num_remaining_opponents = sum([num_at_score for position in all_p0_scores for num_at_score in position])
+    # print(f"{player} {num_remaining_opponents}")
     for position, scores in enumerate(all_player_scores):
         if position == 0:
             # the game starts counting positions from 1
@@ -79,12 +84,12 @@ while True :
                 for roll, roll_count in rolls.items():
                     new_tile = (position + roll - 1) % 10 + 1
                     new_score = score + new_tile
-                    print(f"in {roll_count * num_at_score} universes, player {player} ends up on tile {new_tile} with score {new_score}")
+                    # print(f"in {roll_count * num_at_score} universes, player {player} ends up on tile {new_tile} with score {new_score}")
 
                     if new_score >= END_SCORE:
                         # print(f"{position} {roll} {new_tile} {score}")
-                        # print(f"player {player} has won in {roll_count * num_at_score} more universes. removing them from play")
-                        num_winning_universes[player] += roll_count * num_at_score
+                        # print(f"player {player} has won in {roll_count * num_at_score * num_remaining_opponents} more universes")
+                        num_winning_universes[player] += roll_count * num_at_score * num_remaining_opponents
                     else:
                         new_scores[new_tile][new_score] = roll_count * num_at_score
                 # all_player_scores[position][score] -= num_at_score
@@ -104,7 +109,7 @@ while True :
         and all( [num_at_score == 0 for position in all_p1_scores for num_at_score in position] )
     ):
         break
-    print(num_winning_universes)
+    # print(num_winning_universes)
 
 
 print(num_winning_universes)
