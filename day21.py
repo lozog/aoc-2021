a1b83128896ffae0c36d0855912793b6d1a57d12
Liam Ozog <liam@ada.support>
2021-12-24 11:36:08 -0500

    day 21 star 2 - stolen from reddit

--- day21.py
+++ day21.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
+from functools import lru_cache
+import itertools
 from pprint import pprint
 import re
 
-input_file = open('input/day21_test', 'r')
+input_file = open('input/day21_full', 'r')
 lines = input_file.read().splitlines()
 input_file.close()
 
@@ -51,73 +53,27 @@ for i in range(3):
             rolls[i+j+k+3] += 1
 rolls = dict(rolls)
 
-print(players)
+# mostly stolen from reddit
+@lru_cache(maxsize=None)
+def dirac(p1, p2, p1_score, p2_score, win_score):
+    num_wins = [0, 0]
+    for roll1 in itertools.product([1,2,3], repeat=3):
+        for roll2 in itertools.product([1,2,3], repeat=3):
+            p1_ = (p1 + sum(roll1) - 1 ) % 10 + 1
+            p1_score_ = p1_score + p1_
+            if p1_score_ >= win_score:
+                num_wins[0] += 1
+                break
+            p2_ = (p2 + sum(roll2) - 1) % 10 + 1
+            p2_score_ = p2_score + p2_
+            if p2_score_ >= win_score:
+                num_wins[1] += 1
+                continue
+            subsequent_wins = dirac(p1_, p2_, p1_score_, p2_score_, win_score)
+            num_wins[0] += subsequent_wins[0]
+            num_wins[1] += subsequent_wins[1]
+    return num_wins
 
-END_SCORE = 7
+res = dirac(players[0], players[1], 0, 0, 21)
 
-# 2d array of all possible positions and all possbile scores
-# e.g. the value at all_p1_scores[x][y] is the # of universes where p1 has score y at position x after a certain number of turns
-all_p0_scores = [[0 for i in range(END_SCORE)] for position in range(10+1)]
-all_p1_scores = [[0 for i in range(END_SCORE)] for position in range(10+1)]
-
-all_p0_scores[players[0]][0] = 1
-all_p1_scores[players[1]][0] = 1
-
-num_winning_universes = [0 for player in players]
-player = 0
-all_player_scores = all_p0_scores
-while True:
-    # print(player)
-    new_scores = [[0 for i in range(END_SCORE)] for position in range(10+1)]
-    if player == 0:
-        num_remaining_opponents = sum([num_at_score for position in all_p1_scores for num_at_score in position])
-    else:
-        num_remaining_opponents = sum([num_at_score for position in all_p0_scores for num_at_score in position])
-    # print(f"{player} {num_remaining_opponents}")
-    for position, scores in enumerate(all_player_scores):
-        if position == 0:
-            # the game starts counting positions from 1
-            continue
-        for score, num_at_score in enumerate(scores):
-            if num_at_score > 0:
-                # print(f"{position} {num_at_score}")
-                for roll, roll_count in rolls.items():
-                    new_tile = (position + roll - 1) % 10 + 1
-                    new_score = score + new_tile
-                    # print(f"in {roll_count * num_at_score} universes, player {player} ends up on tile {new_tile} with score {new_score}")
-
-                    if new_score >= END_SCORE:
-                        # print(f"{position} {roll} {new_tile} {score}")
-                        # print(f"player {player} has won in {roll_count * num_at_score * num_remaining_opponents} more universes")
-                        num_winning_universes[player] += roll_count * num_at_score * num_remaining_opponents
-                    else:
-                        new_scores[new_tile][new_score] = roll_count * num_at_score
-                # all_player_scores[position][score] -= num_at_score
-
-    pprint(new_scores)
-    if player == 0:
-        all_p0_scores = new_scores
-        player = 1
-        all_player_scores = all_p1_scores
-    else:
-        all_p1_scores = new_scores
-        player = 0
-        all_player_scores = all_p0_scores
-
-    if (
-        all( [num_at_score == 0 for position in all_p0_scores for num_at_score in position] )
-        and all( [num_at_score == 0 for position in all_p1_scores for num_at_score in position] )
-    ):
-        break
-    # print(num_winning_universes)
-
-
-print(num_winning_universes)
-# pprint(all_p1_scores)
-
-# print(players)
-# print(scores)
-# loser_score = list(filter(lambda x: x < END_SCORE, scores))[0]
-
-# res = loser_score * dice_roll_count
-# print(res)
+print(res)
